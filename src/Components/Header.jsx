51cb8b1a7c9b32d74d630aefe05bce4606f710fb
alexMetan <dudkaoleksandr98@gmail.com>
2022-02-12 19:40:37 +0100

    First commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img src="https://alexdev.pl/img/logo.svg"/>
+        </header>
+    );
+}
+
+export default Header;
